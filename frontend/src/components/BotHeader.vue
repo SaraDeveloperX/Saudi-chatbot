debbc28582232cc0d2b9fbef5acbb3ae0d460727
Sara <saraalmsouud@gmail.com>
2026-01-28 05:53:24 +0300

    chore: remove debug and verification files

--- frontend/src/components/BotHeader.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="absolute top-[60px] left-1/2 -translate-x-1/2 flex flex-col items-center z-20">
-    <div class="w-[56px] h-[56px] rounded-full overflow-hidden border-[2px] border-white shadow-sm mb-2">
-      <img src="@/assets/bot.jpg" alt="Bot" class="w-full h-full object-cover" />
-    </div>
-    <span class="text-white text-sm font-medium opacity-90">Bot</span>
-  </div>
-</template>
